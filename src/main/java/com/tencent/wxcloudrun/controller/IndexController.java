9a06d705a73f8ce2a7b0a820229b7d5241fee2d5
风火火 <446100702@qq.com>
2022-03-15 18:21:19 +0800

    feat: 验证

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,5 +1,11 @@
 package com.tencent.wxcloudrun.controller;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,16 +13,39 @@ import org.springframework.web.bind.annotation.GetMapping;
  * index控制器
  */
 @Controller
-
 public class IndexController {
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
+    /**
+     * 主页页面
+     *
+     * @return API response html
+     */
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/checkSignature")
+    public String checkSignature(String signature, String timestamp, String nonce, String echostr)
+        throws NoSuchAlgorithmException {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add(signature);
+        strings.add(timestamp);
+        strings.add(nonce);
+
+        List<String> collect = strings.stream()
+            .sorted()
+            .collect(Collectors.toList());
+
+        MessageDigest instance = MessageDigest.getInstance("SHA-1");
+
+        String s = Hex.encodeHexString(instance.digest(String.join("", collect).getBytes()));
+
+        if (s.equals(signature)) {
+            return echostr;
+        }
+        return "";
+    }
 
 }
